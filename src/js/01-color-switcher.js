ccce08badb8bd2253eea9218d3c76ccaab9a5286
Mari <40849185+marichiiin@users.noreply.github.com>
2024-06-08 09:13:07 +0800

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
 
+let timerId = null;
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+console.log(startBtn);
+
+startBtn.addEventListener('click', changebg);
+stopBtn.addEventListener('click', stopchangebg);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function changebg() {
+    startBtn.setAttribute('disabled', '');
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    
+}
+
+function stopchangebg() {
+    startBtn.removeAttribute('disabled');
+    clearInterval(timerId);
+}
